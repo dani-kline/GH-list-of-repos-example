d16ee73b5f6f4a6f9a91f97bf39c9a4f9f9d8853
Dani Kline <danikline@Danis-MacBook-Pro.local>
2022-05-20 12:43:59 -0400

    Example for pulling large list of repos from GH

---
+++ apicall.py
@@ -0,0 +1,18 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+git_token = os.getenv("GIT_TOKEN") # git_token is gonna be a Personal Access Token
+
+url = "https://api.github.com/orgs/ORG_NAME_HERE/repos?simple=yes&per_page=100&page=1"
+res=requests.get(url,headers={"Authorization": git_token})
+repos=res.json()
+#adds the following pages to repos
+while 'next' in res.links.keys():
+  res=requests.get(res.links['next']['url'],headers={"Authorization": git_token})
+  repos.extend(res.json())
+#print list of repos
+for rep in repos:
+  print(rep['name'])
